3441da3d88f30ed2809f9938ed8809d33de4d1e6
Emil Mubarqui <emilmubarqui@gmail.com>
2022-08-21 12:16:06 -0500

    añade funcion para promediar regiones de pixeles

--- Dados.py
+++ Dados.py
@@ -23,3 +23,35 @@ def grey(Nom):
 			Ndata[i,j] = (data[i,j,0]+data[i,j,1]+data[i,j,2])/3
 
 	return Ndata
+
+def avgPixel(Imagen,region = 3):
+	'''
+	Promedia la intensidad de los pixeles en una region cuadrada
+
+	Argumentos:
+		Imagen: Arreglo con los valores de los pixeles de la imagen
+		region: tamano de la region a promediar
+
+	Returns:
+
+	'''
+
+	Im_avg = np.zeros((len(Imagen)//region,len(Imagen[0])//region)) # Crea la matriz que contiene los promedios
+
+	for px_ver in range(len(Im_avg)):
+		for px_hor in range(len(Im_avg[0])):
+
+			Im_avg[px_ver,px_hor] = np.average(Imagen[px_ver*region:region*(px_ver + 1), px_hor*region:region*(px_hor + 1)])
+
+	return Im_avg
+
+
+if __name__ == '__main__':
+	Nom = "patos.jpg"
+
+	Im_gris = grey(Nom)
+	Im_avg = avgPixel(Im_gris, region = 20)
+
+	print(Im_avg.shape)
+	Im = Image.fromarray(Im_avg)
+	Im.show()
